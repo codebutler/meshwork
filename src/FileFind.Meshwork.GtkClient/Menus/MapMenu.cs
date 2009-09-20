9b67f944b2aa1ec30352756f566ea3f3497732b6
Eric Butler <eric@extremeboredom.net>
2009-09-19 21:39:28 -0700

    Fix map menu browse item.

--- src/FileFind.Meshwork.GtkClient/Menus/MapMenu.cs
+++ src/FileFind.Meshwork.GtkClient/Menus/MapMenu.cs
@@ -26,11 +26,10 @@ namespace FileFind.Meshwork.GtkClient
 		{
 			mnuMap.Popup ();
 		}
-
 			
 		public void on_mnuNewConnection_activate (object o, EventArgs e) {
 			ConnectDialog connectWindow = new ConnectDialog();
-			connectWindow.Run ();
+			connectWindow.Run();
 		}
 
 		public void on_mnuNewMemo_activate (object o, EventArgs e)
@@ -44,13 +43,12 @@ namespace FileFind.Meshwork.GtkClient
 		public void on_mnuJoinChat_activate (object o, EventArgs e)
 		{
 			JoinChatroomDialog dialog = new JoinChatroomDialog ();		
-			dialog.Run ();
+			dialog.Run();
 		}
 
 		public void on_mnuBrowseFiles_activate (object o, EventArgs e)
 		{
-			//XXX:Gui.BrowserWindow.Show();
-			throw new NotImplementedException();
+			Gui.MainWindow.SelectedPage = UserBrowserPage.Instance;
 		}
 	}
 }
