e192dedc33dc174419ec089e007b9a2fc4977863
Eric Butler <eric@extremeboredom.net>
2009-09-27 13:17:47 -0700

    Don't freeze UI if DNS is slow in ConnectDialog.

--- src/FileFind.Meshwork.GtkClient/Windows/ConnectDialog.cs
+++ src/FileFind.Meshwork.GtkClient/Windows/ConnectDialog.cs
@@ -28,57 +28,55 @@ namespace FileFind.Meshwork.GtkClient
 		ListStore store;
 		ListStore networksListStore;
 
-		[Widget] Button btnConnect;
+		[Widget] Button connectButton;
 		[Widget] ComboBoxEntry ipCombo;
 		[Widget] ComboBox networksComboBox;
-		
-		public ConnectDialog () : base (Gui.MainWindow.Window, "ConnectDialog")
+
+		public ConnectDialog () : base(Gui.MainWindow.Window, "ConnectDialog")
 		{
-			store = new ListStore (typeof(object), typeof(string));
+			store = new ListStore(typeof(object), typeof(string));
 			ipCombo.Model = store;
 			ipCombo.TextColumn = 1;
 			
-			CellRendererPixbuf imageCell = new CellRendererPixbuf ();
+			CellRendererPixbuf imageCell = new CellRendererPixbuf();
 			CellRendererText textCell = new CellRendererText();
-			ipCombo.Clear ();
-			ipCombo.PackStart (imageCell, false);
-			ipCombo.PackStart (textCell, true);
-			ipCombo.SetCellDataFunc (imageCell, ShowImage);
-			ipCombo.SetCellDataFunc (textCell, ShowText);
+			ipCombo.Clear();
+			ipCombo.PackStart(imageCell, false);
+			ipCombo.PackStart(textCell, true);
+			ipCombo.SetCellDataFunc(imageCell, ShowImage);
+			ipCombo.SetCellDataFunc(textCell, ShowText);
 			ipCombo.WrapWidth = 3;
 			ipCombo.Entry.ActivatesDefault = true;
 			PopulateAddressCombo();
-
-			networksListStore = new ListStore (typeof (object));
-			networksListStore.AppendValues (new object());
+			
+			networksListStore = new ListStore(typeof(object));
+			networksListStore.AppendValues(new object());
 			foreach (Network network in Core.Networks) {
-				networksListStore.AppendValues (network);
+				networksListStore.AppendValues(network);
 			}
 			
-			networksComboBox.Clear ();
+			networksComboBox.Clear();
 			
-			CellRendererText networkNameCell = new CellRendererText ();
-			networksComboBox.PackStart (networkNameCell, false);
-			networksComboBox.SetCellDataFunc (networkNameCell, new CellLayoutDataFunc (NetworkTextFunc));
+			CellRendererText networkNameCell = new CellRendererText();
+			networksComboBox.PackStart(networkNameCell, false);
+			networksComboBox.SetCellDataFunc(networkNameCell, new CellLayoutDataFunc(NetworkTextFunc));
 			networksComboBox.Model = networksListStore;
-			networksComboBox.Changed += delegate {
-				PopulateAddressCombo();
-			};
+			networksComboBox.Changed += delegate { PopulateAddressCombo(); };
 			networksComboBox.Active = Math.Min(networksListStore.IterNChildren(), 1);
-
-			userImage = Gui.LoadIcon (16, "stock_person");
-			notImage = Gui.LoadIcon (16, "stock_not");
-			localImage = Gui.LoadIcon (16, "stock_channel");
+			
+			userImage = Gui.LoadIcon(16, "stock_person");
+			notImage = Gui.LoadIcon(16, "stock_not");
+			localImage = Gui.LoadIcon(16, "stock_channel");
 		}
-	
+
 		private void PopulateAddressCombo ()
 		{
-			store.Clear ();
-
+			store.Clear();
+			
 			TreeIter iter;
-			if (networksComboBox.GetActiveIter (out iter)) {
-				Network selectedNetwork = networksListStore.GetValue (iter, 0) as Network;
-
+			if (networksComboBox.GetActiveIter(out iter)) {
+				Network selectedNetwork = networksListStore.GetValue(iter, 0) as Network;
+				
 				/*
 				if (Core.ZeroconfManager != null) {
 					foreach (NearbyNode node in Core.NearbyNodes) {
@@ -92,10 +90,10 @@ namespace FileFind.Meshwork.GtkClient
 					}
 				}
 				*/
-				
+
 				foreach (TrustedNodeInfo node in selectedNetwork.TrustedNodes.Values) {
 					if (node.AllowConnect) {
-						if (!IsNearby (node)) {
+						if (!IsNearby(node)) {
 							IDestination destination = node.FirstConnectableDestination;
 							if (destination != null) {
 								store.AppendValues(node, node.FirstConnectableDestination.ToString());
@@ -103,11 +101,11 @@ namespace FileFind.Meshwork.GtkClient
 						}
 					}
 				}
-
+				
 				if (store.IterNChildren() == 0) {
-					store.AppendValues (new object(), String.Empty);
+					store.AppendValues(new object(), String.Empty);
 				}
-
+				
 				ipCombo.Sensitive = true;
 			} else {
 				ipCombo.Sensitive = false;
@@ -125,111 +123,133 @@ namespace FileFind.Meshwork.GtkClient
 					}
 				}
 			}
-			*/
+			*/			
 			return false;
 		}
-	
-		private void ShowText (CellLayout cell_layout, CellRenderer cell,
-	                              TreeModel tree_model, TreeIter iter)
-		{
-		     object currentRow = tree_model.GetValue (iter, 0);
-			string address = (string)tree_model.GetValue(iter, 1);
 
-		      if (currentRow is TrustedNodeInfo) {
+		private void ShowText (CellLayout cell_layout, CellRenderer cell, TreeModel tree_model, TreeIter iter)
+		{
+			object currentRow = tree_model.GetValue(iter, 0);
+			string address = (string)tree_model.GetValue(iter, 1);
+			
+			if (currentRow is TrustedNodeInfo) {
 				TrustedNodeInfo node = (currentRow as TrustedNodeInfo);
-				((CellRendererText)cell).Markup = String.Format ("{0}\n<span size=\"small\">({1})</span>",
-				                                               node.Identifier, address);
+				((CellRendererText)cell).Markup = String.Format("{0}\n<span size=\"small\">({1})</span>", node.Identifier, address);
 				cell.Sensitive = true;
-		       } else if (currentRow is NearbyNode) {
+			} else if (currentRow is NearbyNode) {
 				NearbyNode node = (currentRow as NearbyNode);
-				((CellRendererText)cell).Markup = String.Format ("{0}\n<span size=\"small\">({1})</span>",
-				                                                node.NickName,
-										address);
+				((CellRendererText)cell).Markup = String.Format("{0}\n<span size=\"small\">({1})</span>", node.NickName, address);
 				cell.Sensitive = true;
-		       } else {
+			} else {
 				((CellRendererText)cell).Markup = "<b>There are no friends to connect to.</b>";
 				cell.Sensitive = false;
-		       }
-	       } 
-	       
-	       private void ShowImage (CellLayout cell_layout, CellRenderer cell, TreeModel tree_model, TreeIter iter)
-	       {
-		       object currentRow = tree_model.GetValue (iter, 0);
+			}
+		}
 
-		       if (currentRow != null) {
+		private void ShowImage (CellLayout cell_layout, CellRenderer cell, TreeModel tree_model, TreeIter iter)
+		{
+			object currentRow = tree_model.GetValue(iter, 0);
+			
+			if (currentRow != null) {
 				if (currentRow is TrustedNodeInfo)
-					((CellRendererPixbuf)cell).Pixbuf = userImage;
-				else if (currentRow is NearbyNode)
+					((CellRendererPixbuf)cell).Pixbuf = userImage; else if (currentRow is NearbyNode)
 					((CellRendererPixbuf)cell).Pixbuf = localImage;
 				else
 					((CellRendererPixbuf)cell).Pixbuf = notImage;
-		       }
-	       }
-		
+			}
+		}
+
 		private void on_connectButton_clicked (object sender, EventArgs e)
 		{
 			try {
+				if (ipCombo.Entry.Text.Trim() == String.Empty) {
+					Gui.ShowErrorDialog("Invalid address", base.Dialog);
+					return;
+				}
+				
+				Network selectedNetwork = null;
+				
+				TreeIter iter;
+				if (networksComboBox.GetActiveIter(out iter)) {
+					selectedNetwork = networksListStore.GetValue(iter, 0) as Network;
+				} 
+				
+				if (selectedNetwork == null) {
+					Gui.ShowErrorDialog("No network selected.", Dialog);
+					return;
+				}
+				
 				int port = TcpTransport.DefaultPort;
 				string address = (ipCombo.Child as Gtk.Entry).Text;
 				IPAddress ip = IPAddress.Any;
-
-				if (!IPAddress.TryParse (address, out ip)) {
 				
+				if (!IPAddress.TryParse(address, out ip)) {
+					
 					// XXX: Support IPv6 w/ port syntax: "[address]:port"
 					
-					if (address.IndexOf (":") > -1) {
-						port = Convert.ToInt32 (address.Substring (address.LastIndexOf (":") + 1));
-						address = address.Substring (0, address.LastIndexOf (":"));
-						LoggingService.LogDebug(port + " " + address);
+					if (address.IndexOf(":") > -1) {
+						port = Convert.ToInt32(address.Substring(address.LastIndexOf(":") + 1));
+						address = address.Substring(0, address.LastIndexOf(":"));
 					}
 					
-					if (!IPAddress.TryParse (address, out ip)) {
-						try {
-							ip = System.Net.Dns.GetHostAddresses(address)[0];
-						} catch (Exception) {
-							Gui.ShowMessageDialog ("Unable to resolve hostname.", Dialog);
-							return;
-						}
+					if (!IPAddress.TryParse(address, out ip)) {		
+						connectButton.Sensitive = false;
+						base.Dialog.GdkWindow.Cursor = new Cursor(CursorType.Watch);
+						System.Threading.ThreadPool.QueueUserWorkItem(delegate {
+							try {
+								ip = System.Net.Dns.GetHostAddresses(address)[0];									
+							} catch (Exception) {
+								ip = null;
+							}
+							Application.Invoke(delegate {
+								connectButton.Sensitive = true;
+								base.Dialog.GdkWindow.Cursor = new Cursor(CursorType.LeftPtr);	
+								Connect(selectedNetwork, address, ip, port);
+							});
+						});
+						
+						return;						
 					}
 				}
-
-				try {
-					TreeIter iter;
-					if (networksComboBox.GetActiveIter (out iter)) {
-						Network selectedNetwork = networksListStore.GetValue (iter, 0) as Network;
-						if (selectedNetwork != null) {
-							ITransport transport = new TcpTransport (ip, port, ConnectionType.NodeConnection);
-							selectedNetwork.ConnectTo (transport);
-						} else {
-							Gui.ShowMessageDialog ("No network selected.", Dialog);
-							return;
-						}
-					} else {
-						Gui.ShowMessageDialog ("No network selected.", Dialog);
-						return;
-					}
-				}
-				catch (Exception ex) {
-					LoggingService.LogError(ex);
-					Gui.ShowMessageDialog (ex.ToString(), Dialog);
-					return;
-				}
-			
-				if (Gui.Settings.RecentConnections.IndexOf(address) != -1)
-					Gui.Settings.RecentConnections.Remove(address);
-		
-				Gui.Settings.RecentConnections.Insert(0, address);
-				Gui.Settings.SaveSettings();
 				
-				Dialog.Respond ((int)ResponseType.Ok);
+				Connect(selectedNetwork, address, ip, port);
+			
 			} catch (Exception ex) {
-				Gui.ShowMessageDialog (ex.Message);
+				LoggingService.LogError(ex);
+				Gui.ShowErrorDialog(ex.Message, base.Dialog);
 			}
 		}
 		
+		private void Connect (Network network, string address, IPAddress ip, int port)
+		{
+			try {
+				if (!base.Dialog.Visible)
+					return;
+				
+				if (ip == null) {
+					Gui.ShowErrorDialog("Unable to resolve hostname.", Dialog);
+					return;
+				}
+				
+				ITransport transport = new TcpTransport(ip, port, ConnectionType.NodeConnection);
+				network.ConnectTo(transport);
+				
+				if (Gui.Settings.RecentConnections.IndexOf(address) != -1)
+					Gui.Settings.RecentConnections.Remove(address);
+				
+				Gui.Settings.RecentConnections.Insert(0, address);
+				Gui.Settings.SaveSettings();
+				
+				Dialog.Respond((int)ResponseType.Ok);
+			} catch (Exception ex) {
+				LoggingService.LogError(ex);
+				Gui.ShowErrorDialog(ex.Message, base.Dialog);
+			}
+		}
+
 		private void NetworkTextFunc (CellLayout layout, CellRenderer cell, TreeModel model, TreeIter iter)
 		{
-			Network network = (model.GetValue (iter, 0) as Network);
+			Network network = (model.GetValue(iter, 0) as Network);
 			if (network != null) {
 				(cell as CellRendererText).Text = network.NetworkName;
 				(cell as CellRendererText).Sensitive = true;
